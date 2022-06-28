2781e7cf2176d997a0565fdc1f3fe19ed2a15361
KgaugeloEliasMatlou <eliasmatlou66@gmail.com>
2022-06-28 10:54:36 +0200

    Add project files.

---
+++ Async_And_Await_Example/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class program
+{
+    static void Main()
+    {
+        asyncAwaitdemo demo = new asyncAwaitdemo();
+        demo.Dostuff();
+        for(int i = 0; i < 100; i++)
+        {
+            Console.WriteLine("Working on the Main Thread....");
+        }
+
+    }
+}
+public class asyncAwaitdemo
+{
+
+    public async Task Dostuff()
+    {
+        //await Task.Run(() => 
+        //{
+        CountToFifty();
+
+        //});
+
+        //This will not execute until countTofifty has Completed
+
+        Console.WriteLine("counting to 50 is completed ...");
+    }
+    public async Task<string> CountToFifty()
+    {
+        int counter;
+        for(counter = 0; counter < 51; counter++)
+        {
+            Console.WriteLine("BG thread "+counter);
+        }
+    }
+}
